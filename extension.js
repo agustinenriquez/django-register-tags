f4920287904c78c85caff4d969ba534def5ae9fa
Agustin Enriquez <agustin_enriquez@hotmail.com>
2024-08-29 19:07:12 -0300

    Update extension.js
    improved but untested

--- extension.js
+++ extension.js
@@ -3,147 +3,119 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-
 function activate(context) {
     console.log('Congratulations, your extension "django-register-tags" is now active!');
-    let boilerPlate = function(word) {
-        // i know this looks awful but it seems to be the way VSCODE recognizes
-        // the position to inject the string.
-        return `
 
+    const boilerPlate = (word) => `
 @admin.register(${word})
 class ${word}Admin(admin.ModelAdmin):
     class Meta:
         model = ${word}
-        fields = '__all__'\n`;
-}
+        fields = '__all__'
+`;
 
-    let modelsBoilerPlate = function(className) {
-        return `
-
+    const modelsBoilerPlate = (className) => `
 @admin.register(${className})
 class ${className}Admin(admin.ModelAdmin):
     class Meta:
         model = ${className}
-        `;
-    }
+`;
 
-    let listDisplay = function(name, fields) {
-        return `
-
+    const listDisplay = (name, fields) => `
 @admin.register(${name})
 class ${name}Admin(admin.ModelAdmin):
-    list_display = ("${fields}")\n`;
-    }
+    list_display = (${fields.map(field => `"${field}"`).join(', ')})
+`;
 
-    let isAdminFile = function() {
-        var currentFilePath = vscode.window.activeTextEditor.document.fileName;
-        currentFilePath = currentFilePath.split('/') 
-        currentFilePath = currentFilePath[currentFilePath.length-1]
-        if (currentFilePath == "admin.py") return true
-    }
+    const isAdminFile = () => {
+        const currentFilePath = vscode.window.activeTextEditor.document.fileName.split('/').pop();
+        return currentFilePath === "admin.py";
+    };
 
-    let hasModelsImports = function(afileText) {
-        if (afileText.indexOf('from .models import') > 0) return true
-    }
+    const hasModelsImports = (fileText) => fileText.includes('from .models import');
 
-    let registerModel = vscode.commands.registerCommand('django-register-tags.registerModel', function () {
-        var editor = vscode.window.activeTextEditor;
-        var selection = editor.selection;
-        var word = editor.document.getText(selection);
-        if (isAdminFile()) {
-            if (word) {
-                // Add validation for model names (ie: first letter must be capitalized.)
-                var lineCount = editor.document.lineCount;
-                var position = new vscode.Position(lineCount, 0)
+    const registerModel = vscode.commands.registerCommand('django-register-tags.registerModel', () => {
+        const editor = vscode.window.activeTextEditor;
+        const selection = editor.selection;
+        const word = editor.document.getText(selection);
+
+        if (isAdminFile() && word) {
+            // Validate model name (must start with a capital letter)
+            if (word.charAt(0).toUpperCase() === word.charAt(0)) {
+                const lineCount = editor.document.lineCount;
+                const position = new vscode.Position(lineCount, 0);
+
                 editor.edit(editBuilder => {
-                    editBuilder.insert(position, boilerPlate(word))
+                    editBuilder.insert(position, boilerPlate(word));
                 });
-            };
-        };
-
-        vscode.window.showInformationMessage(boilerPlate);
-    });
-
-    let registerModels = vscode.commands.registerCommand('django-register-tags.registerModels', function () {
-        var editor = vscode.window.activeTextEditor;
-        var fileText = editor.document.getText();
-        var codeToInject = "";
-        var modelString = "";
-        if (isAdminFile()) {
-            if (hasModelsImports(fileText)) {
-                fileText.splitLines().forEach((e) => {
-                    if (e.indexOf('.models') > 0) {
-                        var indexOfImport = e.indexOf('import') + 7;
-                        modelString = e.substring(indexOfImport, e.length);
-                        modelString = modelString.replace(/,+/g, '').split(' ');
-                        modelString.forEach((model) => {
-                            codeToInject += boilerPlate(model)
-                        });
-                        var lineCount = editor.document.lineCount;
-                        var position = new vscode.Position(lineCount, 0)
-                        editor.edit(editBuilder => {
-                            editBuilder.insert(position, codeToInject)
-                        });
-                    };
-                });
+            } else {
+                vscode.window.showWarningMessage("Model name should start with a capital letter.");
             }
         }
-        vscode.window.showInformationMessage(boilerPlate);
     });
 
-    context.subscriptions.push(registerModels);
+    const registerModels = vscode.commands.registerCommand('django-register-tags.registerModels', () => {
+        const editor = vscode.window.activeTextEditor;
+        const fileText = editor.document.getText();
 
-    let registerModelByField = vscode.commands.registerCommand('django-register-tags.registerModelByField', function () {
-        var editor = vscode.window.activeTextEditor;
-        var currentFilePath = vscode.window.activeTextEditor.document.fileName;
-        var modelsFilePath = currentFilePath.replace('admin', 'models');
-        var selectedClassName = editor.document.getText(editor.selection);
-        var className = null;
-        let classProps = {};
-        let properties = []
-        var openPath = vscode.Uri.file(modelsFilePath);
-        vscode.workspace.openTextDocument(openPath).then(text => {
-            let textString = text.getText();
-            let splittedText = textString.splitLines();
-            let hasClass = false;
-            splittedText.forEach((line)=>{
-                if (!(line.indexOf('def') == 0) && line.indexOf('return') == -1) {
-                    if (hasClass && line.indexOf('class') >=0) {
-                        hasClass = false;
-                    }
-                    if (!hasClass && line.indexOf('class') >= 0) {
-                        className = line.split(' ')[1].split('(')[0];
-                        if (className == selectedClassName) {
-                            classProps[className] = [];
-                            hasClass = true;
-                            modelsBoilerPlate(className)
-                        }
-                    }
-                    if (hasClass && line.indexOf('=') > 0 && className == selectedClassName) {
-                        let property = line.split('=')[0].replace(/\s/g,'')
-                        classProps[className].unshift(property);
-                        properties.unshift(property);
-                    }
+        if (isAdminFile() && hasModelsImports(fileText)) {
+            let codeToInject = "";
+            fileText.splitLines().forEach(line => {
+                if (line.includes('.models')) {
+                    const models = line.split('import')[1].trim().split(/\s*,\s*/);
+                    models.forEach(model => {
+                        codeToInject += boilerPlate(model);
+                    });
+
+                    const lineCount = editor.document.lineCount;
+                    const position = new vscode.Position(lineCount, 0);
+                    editor.edit(editBuilder => {
+                        editBuilder.insert(position, codeToInject);
+                    });
                 }
             });
-            let codeToInject = listDisplay(selectedClassName, properties.toString().split(',').join('", "'));
-            var lineCount = editor.document.lineCount;
-            var position = new vscode.Position(lineCount, 0)
-            editor.edit(editBuilder => {
-                editBuilder.insert(position, codeToInject)
-            });
-        });
-        vscode.window.showTextDocument(text);
-        
+        }
     });
 
+    const registerModelByField = vscode.commands.registerCommand('django-register-tags.registerModelByField', () => {
+        const editor = vscode.window.activeTextEditor;
+        const currentFilePath = editor.document.fileName;
+        const modelsFilePath = currentFilePath.replace('admin', 'models');
+        const selectedClassName = editor.document.getText(editor.selection);
+        const openPath = vscode.Uri.file(modelsFilePath);
+
+        vscode.workspace.openTextDocument(openPath).then(document => {
+            const textLines = document.getText().split('\n');
+            let className = null;
+            let properties = [];
+
+            textLines.forEach(line => {
+                if (line.startsWith('class ')) {
+                    className = line.split(' ')[1].split('(')[0];
+                } else if (className === selectedClassName && line.includes('=')) {
+                    const property = line.split('=')[0].trim();
+                    properties.push(property);
+                }
+            });
+
+            if (className === selectedClassName && properties.length > 0) {
+                const codeToInject = listDisplay(className, properties);
+                const lineCount = editor.document.lineCount;
+                const position = new vscode.Position(lineCount, 0);
+
+                editor.edit(editBuilder => {
+                    editBuilder.insert(position, codeToInject);
+                });
+            }
+        });
+    });
+
+    context.subscriptions.push(registerModel, registerModels, registerModelByField);
 }
-exports.activate = activate;
 
 function deactivate() {}
 
 module.exports = {
     activate,
     deactivate
-}
+};
